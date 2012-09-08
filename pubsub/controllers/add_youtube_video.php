702176b979c702f174c8ea7f28412d6224d8a604
natestarner <nate.starner@gmail.com>
2012-09-08 12:41:49 -0400

    now finding all correct videos on youtube and writing to db

--- pubsub/controllers/add_youtube_video.php
+++ pubsub/controllers/add_youtube_video.php
@@ -3,6 +3,45 @@ function add_youtube_video(){
 	global $config;
 	require_once $config["path_to_default_site_module"] . "/models/db.php";
 	require_once $config["path_to_default_site_module"] . "/models/video.php";
+	
+	$ch = curl_init("https://gdata.youtube.com/feeds/api/videos/-/preachcaster?alt=json");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	$json_string_data = curl_exec($ch);
+	curl_close($ch);
+	$video_data = json_decode($json_string_data,true);
+
+	$db_conn = new db($config["db_connection"]);
+	$db_resource = $db_conn->get_resource();
+	$video = new Video($db_resource);
+	
+	foreach($video_data["feed"]["entry"] as $single_video){
+		//parse out ID
+		$tmp_array = explode("/",$single_video["id"]['$t']);
+		$video_id = $tmp_array[count($tmp_array) - 1];
+		//rxBS1E0KZQU
+		//CQzUsTFqtW0
+		$video_params = array(
+			"video_id" => $video_id
+			,"author" => $single_video["author"][0]["name"]['$t']
+			,"title" => $single_video["title"]['$t']
+			,"description" => $single_video["content"]['$t']
+			,"date_recorded" => isset($single_video['yt$recorded']) ? $single_video['yt$recorded']['$t'] : null
+			,"location" => isset($single_video['yt$location']) ? $single_video['yt$location']['$t'] : null
+			,"url" => isset($single_video['media$group']['media$player']) ? $single_video['media$group']['media$player'][0]["url"] : null
+			,"duration" => isset($single_video['media$group']['yt$duration']) ? $single_video['media$group']['yt$duration']["seconds"] : null
+			,"comment_url" => isset($single_video['gd$comments']) && isset($single_video['gd$comments']['gd$feedLink']) ? $single_video['gd$comments']['gd$feedLink']["href"] : null
+			,"thumbnails" => isset($single_video['media$group']) && isset($single_video['media$group']['media$thumbnail']) ? $single_video['media$group']['media$thumbnail'] : null
+		);
+		$video->add_video($video_params,"YouTube");
+	}
+	
+	die("here");
+	
+}
+/*
+	old zend crap (that works)
+ * 
 	$clientLibraryPath = $config["path_to_zend_gdata_library"];
 	$oldPath = set_include_path(get_include_path() . PATH_SEPARATOR . $clientLibraryPath);
 	require_once 'Zend/Loader.php';
@@ -10,7 +49,7 @@ function add_youtube_video(){
 	Zend_Loader::loadClass('Zend_Gdata_YouTube');
 	$yt = new Zend_Gdata_YouTube();
 	$query = $yt->newVideoQuery();
-	$query->category = 'Comedy/dog';
+	$query->category = 'dog';
 	 
 	echo $query->queryUrl . "\n";
 	$videoFeed = $yt->getVideoFeed($query);
@@ -20,45 +59,26 @@ function add_youtube_video(){
 	$video = new Video($db_resource);
 	
 	foreach ($videoFeed as $videoEntry) {
-		dump($videoEntry);
-		$pdo_params = array(
-			
+		$author_data = $videoEntry->getAuthor();
+		$author_name = $author_data[0]->getName()->getText();
+		$video_data = array(
+			"video_id" => $videoEntry->getVideoId()
+			,"author" => $author_name
+			,"title" => $videoEntry->getVideoTitle()
+			,"description" => $videoEntry->getVideoDescription()
+			,"date_recorded" => $videoEntry->getVideoRecorded()
+			,"location" => $videoEntry->getLocation()
+			,"url" => $videoEntry->getVideoWatchPageUrl()
+			,"duration" => $videoEntry->getVideoDuration()
+			,"private" => $videoEntry->isVideoPrivate() ? 1 : 0
+			//,"tags" => $videoEntry->getVideoTags()
+			,"category" => $videoEntry->getVideoCategory()
+			//,"developer_tags" => $videoEntry->getVideoDeveloperTags()
+			,"state" => $videoEntry->getVideoState()
+			,"comment_url" => $videoEntry->getVideoCommentFeedUrl()
+			,"embeddable" => $videoEntry->getNoEmbed() ? false : true
+			//,"racy" => $videoEntry->getRacy()
 		);
-		//getVideoId()
-		//getVideoRecorded() date the video was recorded
-		//getWhere() geo
-		//getVideoWatchPageUrl() url to you tube video
-		//getVideoDuration()
-		//isVideoPrivate()
-		//getVideoTags()
-		//getVideoViewCount()
-		//getVideoGeoLocation()
-		//getVideoRatingInfo()
-		//getVideoCategory()
-		//getVideoDeveloperTags()
-		//getVideoState()
-		//getVideoCommentFeedUrl()
-		//getRecorded() date that the video was recorded
-		//getLocation() Gets the location where the video was recorded
-		//getNoEmbed() If the return value is an instance of Zend_Gdata_YouTube_Extension_NoEmbed, this video cannot be embedded.
-		//getStatistics()
-		//getRacy()
-		//getRating()
-		
-	    echo "---------VIDEO----------\n";
-	    echo "Title: " . $videoEntry->getVideoTitle() . "\n";
-	    echo "\nDescription:\n";
-	    echo $videoEntry->getVideoDescription();
-	    echo "\n\n\n";
+		$video->add_video($video_data,"YouTube");
 	}
-	/*
-	$ch = curl_init("https://gdata.youtube.com/feeds/api/videos/-/Comedy");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	$data = curl_exec($ch);
-	curl_close($ch);
-	 */
-	//$xml = file_get_contents("https://gdata.youtube.com/feeds/api/videos/-/Comedy");
-	die("here");
-	
-}
+ */
--- site/models/video.php
+++ site/models/video.php
@@ -7,5 +7,57 @@ class Video{
 			$this->db = $db_connection;
 		}
     }
+	
+	public function add_video($video_data=array(),$site=false){
+		if(!$site){
+			throw new Exception('You must supply a site.');
+		}
+		$statement = $this->db->prepare("
+			SELECT site_id
+			FROM site
+			WHERE name = :name");
+		$statement->bindValue(":name", $site, PDO::PARAM_STR);
+  		$statement->execute();
+  		$site_data = $statement->fetch(PDO::FETCH_ASSOC);
+		if(!$site_data){
+			throw new Exception('Site not found.');
+		}
+		$pdo_params = array(
+			(isset($video_data["video_id"])) ? $video_data["video_id"] : null
+			,(isset($video_data["author"])) ? $video_data["author"] : null
+			,(isset($video_data["title"])) ? $video_data["title"] : null
+			,(isset($video_data["description"])) ? $video_data["description"] : null
+			,(isset($video_data["date_recorded"])) ? $video_data["date_recorded"] : null
+			,(isset($video_data["location"])) ? $video_data["location"] : null
+			,(isset($video_data["url"])) ? $video_data["url"] : null
+			,(isset($video_data["duration"])) ? $video_data["duration"] : null
+			,(isset($video_data["comment_url"])) ? $video_data["comment_url"] : null
+			,$site_data["site_id"]
+		);
+		$statement = $this->db->prepare("
+			INSERT IGNORE INTO video
+			(video_id, author, title, description, date_recorded, location, url, duration,comment_url, site_id, date_added)
+			VALUES
+			(?,?,?,?,?,?,?,?,?,?,NOW())");
+  		$statement->execute($pdo_params);
+		
+		if(isset($video_data["thumbnails"]) && $video_data["thumbnails"]){
+			foreach($video_data["thumbnails"] as $single_thumbnail){
+				$thumbnail_params = array(
+					isset($single_thumbnail["url"]) ? $single_thumbnail["url"] : null 
+					,isset($single_thumbnail["height"]) ? $single_thumbnail["height"] : null
+					,isset($single_thumbnail["width"]) ? $single_thumbnail["width"] : null
+					,isset($single_thumbnail["time"]) ? $single_thumbnail["time"] : null
+					,isset($video_data["video_id"]) ? $video_data["video_id"] : null
+				);
+				$statement = $this->db->prepare("
+					INSERT IGNORE INTO thumbnail
+					(url, height, width, time, video_id)
+					VALUES
+					(?,?,?,?,?)");
+		  		$statement->execute($thumbnail_params);
+			}
+		}
+	}
 }
 ?>
